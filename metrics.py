5bab36123875c4069ec39d4ca77a684443efe08e
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2025-03-17 10:58:58 -0400

    finish average and maximum functions in metrics.py

--- metrics.py
+++ metrics.py
@@ -8,14 +8,32 @@ def average(data: list) -> float:
         float: a floating point value representing the average of this list
     """
     average = 0
-    ...
+    sum = 0
+    for _ in data:
+        if type(_) == int or type(_) == float:
+            sum += _
+    if len(data) == 0:
+        return []
+    average = sum / len(data)
+    return average
 
 
 def maximum(data: list) -> float:
     """
-    INSERT DOCSTRING HERE
+    Find the largest number in the list.
+
+    Args:
+        data (list[int]): list of integers representing heart rate samples
+    Returns:
+        float: a floating point value representing the maximum of in the list
     """
-    pass
+    if (len(data) == 0):
+        return []
+    max_num = data[0]
+    for _ in data[1:]:
+        if max_num < _:
+            max_num = _
+    return max_num
 
 
 def variance(data: list) -> float:
