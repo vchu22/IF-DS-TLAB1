6612738f83c0f85d773065fd81437b2744208727
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2025-03-17 10:46:33 -0400

    finish implementing filter_nondigits function

--- cleaner.py
+++ cleaner.py
@@ -1,5 +1,19 @@
 def filter_nondigits(data: list) -> list:
     """
-    INSERT DOCSTRING HERE
+    This function takes in a list as the input and returns an integer list of the valid digits appeared in the input list.
+
+    Args:
+        data (list): A list containing the input data.
+    Returns:
+        list: A list containing only the valid digits from the data list.
     """
-    pass
+
+    # create an empty list
+    result = []
+    # iterate through the data
+    for _ in data:
+        if type(_) == float or type(_) == int:
+            result.append(_)
+        elif _.strip().isdigit():
+            result.append(int(_.strip()))
+    return result
